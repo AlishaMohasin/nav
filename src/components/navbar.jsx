941771981cbbcc07e5185c8cdea323288dfa88c3
AlishaMohasin <alishamohasin85@gmail.com>
2022-05-20 23:05:25 +0530

    changed logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div >
 <div className={styles.main}>
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRewTM_Bw2C_dpE7iD6R1whPabYVDG0-kz8IA&usqp=CAU"/>
+  <h2 className={styles.main1}>LOGOBAKERY</h2>
     {link.map((data)=>(<h4 >{[...data]}</h4>)
     )}
     <button className={styles.b1}>Contact</button>
